0883f692a8f2ba61e81825380102f2ac6b9b5889
Arun-Leo <aruns88843@gmail.com>
2025-11-11 12:52:30 +0530

    Add string comparison function to ExampleStrings contract
    Added a function to compare two strings using keccak256 hashing.

--- ExampleStrings.sol
+++ ExampleStrings.sol
@@ -8,4 +8,7 @@ contract ExampleStrings {
     function setMyString(string memory _myString) public {
         myString = _myString;
     }
+    function compareTwoString(string memory _myString) public view returns(bool) {
+        return keccak256(abi.encode(myString)) == keccak256(abi.encode(_myString));
+    }
 }
